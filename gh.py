ff43864166723bb198006fd0de013c31620a559e
Junaid0-glitch <junaid76170@gmail.com>
2025-05-24 20:14:51 +0530

    Initial commit with model file

---
+++ gh.py
@@ -0,0 +1,7 @@
+import transformers
+import cv2
+import os
+
+print(f"Transformers version: {transformers.__version__}")
+print(f"OpenCV (cv2) version: {cv2.__version__}")
+print(f"Python (os) version: {os.sys.version}")  # os is built-in, shows Python version
